e3022a5e9d989e3d67912d3b7abc7f8d2120342b
priyankadhulkhed <priyanka.dhulkhed@gmail.com>
2019-02-01 19:47:08 -0500

    new files

--- CTCI/Chapter 2/2_1.py
+++ CTCI/Chapter 2/2_1.py
@@ -2,8 +2,55 @@
 
 from LinkedList import LinkedList
 
-head = LinkedList()
-head.add("1")
-head.add("2")
-head.add("3")
-print(head.traverse())
+# With a hashset
+# Run time: O(N)
+# Space Complexity: O(N) in the worst case when no duplicates exist
+def removeDups1(ll):
+    if ll is None or ll.head.next is None:
+        return ll
+    llSet = []
+    prev = ll.head
+    ptr = ll.head.next
+    llSet.append(ll.head.value)
+    while ptr:
+        if ptr.value in llSet:
+            prev.next = ptr.next
+            ptr =  prev.next
+        else:
+            llSet.append(ptr.value)
+            ptr = ptr.next
+            prev = prev.next
+
+# Without a temporary buffer
+# Run time: O(N^2)
+# Space Complexity: O(1)
+def removeDups2(ll):
+    if ll.head == None or ll.head.next == None:
+        return ll
+    ptr = ll.head
+    while ptr:
+        prev = ptr
+        ptrRunner = ptr.next
+        while ptrRunner:
+            if ptrRunner.value == ptr.value:
+                prev.next = ptrRunner.next
+                ptrRunner.next = None
+                ptrRunner = prev.next
+            else:
+                ptrRunner = ptrRunner.next
+                prev = prev.next
+        ptr = ptr.next
+
+
+
+ll = LinkedList()
+ll.generate(10, 0, 9)
+print(ll)
+removeDups1(ll)
+print(ll)
+
+
+ll.generate(10, 0, 9)
+print(ll)
+removeDups2(ll)
+print(ll)
--- CTCI/Chapter 2/2_2.py
+++ CTCI/Chapter 2/2_2.py
@@ -1,2 +1,49 @@
 # Return kth to the last: Implement an algorithm to find the kth to last element of a singly linked list
+# Inputs: Singly ll, k
+# Output: value of kth to last element
 
+from LinkedList import LinkedList
+
+# Find length of ll and then go to len - k element and return that
+# Run time: O(N)
+# Space Complexity: O(1)
+def findK1(ll, k):
+    if ll == None:
+        return None
+    # find length
+    lenLL = 0
+    ptr = ll.head
+    while ptr:
+        lenLL += 1
+        ptr = ptr.next
+    if k >= lenLL:
+        return None
+    count = 1
+    ptr = ll.head
+    while count != lenLL - k:
+        ptr = ptr.next
+        count += 1
+    return ptr.value
+
+# With a runner
+
+def findK2(ll, k):
+    if ll is None:
+        return ll
+    runner = ll.head
+    runnerPosition = 0
+    while runnerPosition != k and runner.next:
+        runner = runner.next
+        runnerPosition += 1
+    ptr = ll.head
+    while runner.next:
+        ptr = ptr.next
+        runner = runner.next
+    return ptr.value
+
+
+
+ll = LinkedList()
+ll.generate(10, 0, 9)
+print(ll)
+print(findK2(ll, 10))
